1751ac6e06d547938b09905b7cc445bbcc49ea4f
denis8sw <denisappleid12@gmail.com>
2024-02-22 13:41:47 +0200

    Creating Navbar Component
    I cleared the default html and created a navbar component.

--- chance/src/app/app.module.ts
+++ chance/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//other
+import { MatIconModule } from '@angular/material/icon'
+
+
+//components
+import { NavComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ chance/src/app/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component, NgModule } from "@angular/core";
+
+
+@Component({
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss'],
+})
+
+export class NavComponent {
+
+}
