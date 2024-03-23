1466d27a57adc166ad1004a3ef08a8f0ff78947e
denis8sw <denisappleid12@gmail.com>
2024-03-23 18:44:13 +0200

    Updating api url

--- chance/src/app/utils/global.ts
+++ chance/src/app/utils/global.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class GlobalVars{
-    apiUrl : string = 'http://localhost:3001'
+    apiUrl : string = 'https://a1f9-77-64-210-32.ngrok-free.app'
     photosUrl : string = this.apiUrl + '/profile_photos/'
     postsUrl : string = this.apiUrl + '/posts/'
 }
