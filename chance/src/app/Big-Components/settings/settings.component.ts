7eea75ebc8224d140c6b274a1519caef5ed1b264
denis8sw <denisappleid12@gmail.com>
2024-03-28 13:36:52 +0200

    Password Change Option
    Users are able to change their password now

--- chance/src/app/Big-Components/settings/settings.component.ts
+++ chance/src/app/Big-Components/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, Renderer2, ElementRef, QueryList, ViewChildren  } from "@angular/core";
+import { Component, Renderer2, ElementRef, QueryList, ViewChildren } from "@angular/core";
 import { UserInfoService } from "src/app/Services/get-userinfo.service";
 import { SettingsService } from "src/app/Services/settings.service";
 
@@ -9,11 +9,56 @@ import { SettingsService } from "src/app/Services/settings.service";
     styleUrls: ['./settings.component.scss'],
 })
 
-export class SettingsComponent{
+export class SettingsComponent {
 
-    isPage : number = 1
+    isPage: number = 1
+    account: number = 0
+
+    expandHeight: number = 0;
+
+    isDefault : number = 0;
+
     body = document.body;
 
-    constructor(public settingsService : SettingsService, public userInfoService : UserInfoService, public renderer: Renderer2) { }
+    constructor(public settingsService: SettingsService, public userInfoService: UserInfoService, public renderer: Renderer2) { }
+
+
+
+    toggleAccountDisplay(num: number, height: number): void {
+        this.expandHeight = height
+        this.account === num ? (this.account = 0) : (this.account = num);
+    }
+
+    getPasswords(value: string, which: number) {
+        this.isDefault = 0
+        switch (which) {
+            case 1:
+              this.settingsService.passwordInfo.oldPassword = value
+                break;
+            case 2:
+                this.settingsService.passwordInfo.newPassword = value
+                break;
+            case 3:
+                this.settingsService.passwordInfo.confirmNewPassword = value
+                break;
+        }
+
+    }
+
+    clearPasswords() {
+        this.settingsService.passwordInfo.oldPassword = ''
+        this.settingsService.passwordInfo.newPassword = ''
+        this.settingsService.passwordInfo.confirmNewPassword = ''
+        this.isDefault = 1;
+    }
+
+    ngAfterContentChecked(): void {
+        if(this.account === 0 && this.isDefault === 0) {
+            this.clearPasswords()
+        }
+    }
+
+
+
 
 }
--- chance/src/app/Services/settings.service.ts
+++ chance/src/app/Services/settings.service.ts
@@ -1,29 +1,45 @@
-import { Injectable} from "@angular/core";
+import { Injectable } from "@angular/core";
 import { BlankService } from "./blank.service";
 import { BehaviorSubject, Observable } from "rxjs";
+import { ErrorSuccessService } from "./error-success.service";
+import { SessionService } from "./session.service";
+import { GlobalVars } from "../utils/global";
 
+interface passwordNewInfo {
+    oldPassword: string;
+    newPassword: string;
+    confirmNewPassword: string;
+}
 
 
 @Injectable()
 export class SettingsService {
 
+    passwordInfo: passwordNewInfo;
 
 
     public isSettings = new BehaviorSubject<boolean>(false);
     isSettings$: Observable<boolean> = this.isSettings.asObservable();
 
     //main settings
-    isDarkTheme : boolean = false
-    isNotifications : boolean = true
+    isDarkTheme: boolean = false
+    isNotifications: boolean = true
 
 
 
-    constructor(private blankService: BlankService) { }
+    constructor(private errorSuccessService: ErrorSuccessService, private sessionService: SessionService, private globalVars: GlobalVars) {
+        this.passwordInfo = {
+            oldPassword: '',
+            newPassword: '',
+            confirmNewPassword: ''
+        }
+    }
 
     toggle() {
         const value = this.isSettings.getValue()
 
         this.isSettings.next(!value)
+
     }
 
     enable() {
@@ -42,6 +58,54 @@ export class SettingsService {
         this.isNotifications = !this.isNotifications
     }
 
- 
+    changePassword(): void {
+        fetch(this.globalVars.apiUrl + '/changePassword', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ passwordInfo: this.passwordInfo, tokenCookie: this.sessionService.getToken() })
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+
+                if (data.error) {
+                    this.errorSuccessService.setError(data.error)
+                    this.errorSuccessService.enableError()
+                } else {
+                    this.passwordInfo = {
+                        oldPassword: '',
+                        newPassword: '',
+                        confirmNewPassword: ''
+                    };
+
+                    this.errorSuccessService.setSuccess(data.message)
+                    this.errorSuccessService.enableSuccess()
+
+                    setTimeout(() => {
+                        location.reload()
+                    }, 1600);
+
+                }
+
+            })
+            .catch(error => {
+                //     this.errorSuccessService.showError('Error changing password.');
+                console.error('Error:', error);
+            });
+    }
+
+    changeEmail() {
+        this.errorSuccessService.setError("You can't change your email")
+        this.errorSuccessService.enableErrorTime(1700)
+    }
+
+    add2FA() {
+        this.errorSuccessService.setError("Coming soon")
+        this.errorSuccessService.enableErrorTime(1700)
+    }
+
 
 }
