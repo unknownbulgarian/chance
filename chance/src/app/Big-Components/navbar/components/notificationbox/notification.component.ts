6947c879110bc99200f30b71ec0e68a129cd2543
denis8sw <denisappleid12@gmail.com>
2024-03-29 08:01:24 +0200

    Chat fix for mobile users
    It should not push the keyboard down

--- chance/src/app/Big-Components/navbar/components/notificationbox/notification.component.ts
+++ chance/src/app/Big-Components/navbar/components/notificationbox/notification.component.ts
@@ -1,7 +1,9 @@
 import { Component, NgModule, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { ChatService } from "src/app/Services/chat.service";
+import { UserInfoService } from "src/app/Services/get-userinfo.service";
 import { GetPostInfoService } from "src/app/Services/getPost-Info.service";
+import { LoadingService } from "src/app/Services/loading.service";
 import { LoopService } from "src/app/Services/loop.service";
 import { NavBarService } from "src/app/Services/navbar.service";
 import { ProfilesService } from "src/app/Services/profiles.service";
@@ -17,7 +19,10 @@ import { GlobalVars } from "src/app/utils/global";
 
 export class NotificationBoxComponent {
 
-    constructor(private profileService : ProfilesService, public settingsService : SettingsService, private getPostInfoService : GetPostInfoService, public chatService : ChatService, public navBarService: NavBarService, public loopService: LoopService, public globalVars: GlobalVars, public router: Router) { }
+    constructor( private loadingService : LoadingService,
+         private userInfoService : UserInfoService, private profileService : ProfilesService, 
+         public settingsService : SettingsService, private getPostInfoService : GetPostInfoService, public chatService : ChatService, 
+         public navBarService: NavBarService, public loopService: LoopService, public globalVars: GlobalVars, public router: Router) { }
 
     isNoti: number = 1;
 
@@ -29,10 +34,12 @@ export class NotificationBoxComponent {
             this.router.navigate(['/profiles/' + notification.username]);
         } else if (notification.type === 'chat_message') {
             this.router.navigate(['/chat']);
+            this.loadingService.miniLoadedSubject.next(0)
             this.loopService.selectedUser = notification.username;
             this.navBarService.currentPhoto.next(notification.profile_photo)
             this.profileService.checkIfFollow(notification.username)
             this.loopService.getChat(notification.username)
+            this.userInfoService.getPublicUserData(notification.username)
             this.loopService.callChat()
             
         } else {
